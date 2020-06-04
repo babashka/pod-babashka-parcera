28aa4b726e914e1cde68c4de687188f37c294463
Michiel Borkent <michielborkent@gmail.com>
2020-06-04 15:46:57 +0200

    README [skip ci]

--- examples/find_comments.clj
+++ examples/find_comments.clj
@@ -4,14 +4,12 @@
 (pods/load-pod "pod-babashka-parcera")
 (require '[pod.babashka.parcera :as parcera])
 
-(doseq [file *command-line-args*]
-  (let [parsed (parcera/ast (slurp file))]
-    (loop [nodes (rest parsed)]
-      (when-first [n nodes]
-        (when (and (= :list (first n))
-                   (= '(:symbol "comment") (second n)))
-          (let [loc (-> (meta n) :parcera.core/start)]
-            (println (str file ":" (:row loc) ":" (:col loc)))
-            (println (parcera/code n))
-            (println)))
-        (recur (rest nodes))))))
+(doseq [file *command-line-args*
+        :let [parsed (parcera/ast (slurp file))]
+        node (rest parsed)]
+  (when (and (= :list (first node))
+             (= '(:symbol "comment") (second node)))
+    (let [loc (-> (meta node) :parcera.core/start)]
+      (println (str file ":" (:row loc) ":" (:col loc)))
+      (println (parcera/code node))
+      (println))))
